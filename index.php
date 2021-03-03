b20d17586afb4466305cea3cf8fb0985e9934d58
Thiago Gomes <thiago.g.t9@hotmail.com>
2021-03-03 16:02:35 -0300

    Alteração de cores

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Formula 1 2021</title>
-</head>
-<body>
-
-<h1>Calendário</h1>
-<p>A temporada 2021 começa no próximo dia 28/03 com muitas novidades</p>
-
-</body>
+   <head>
+      <title>Formula 1 2021</title>
+   </head>
+   <body>
+      <h1>Calendário</h1>
+      <p>A temporada 2021 começa no próximo dia 28/03 com muitas novidades</p>
+      <div style="width: 100%;display:flex;height: 80px">
+         <div style="width:50;backgroud: blue"></div>
+         <div style="width:50;backgroud: red"></div>
+      </div>
+   </body>
 </html>
