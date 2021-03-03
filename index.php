e46d7be82ec586ea0b0cabc6b625ff8df660fb33
Thiago Gomes <thiago.g.t9@hotmail.com>
2021-03-03 14:25:06 -0300

    versionamento para o meu primeiro site em produção

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Formula 1 2021</title>
+</head>
+<body>
+
+<h1>Calendário</h1>
+<p>A temporada 2021 começa no próximo dia 28/03 com muitas novidades</p>
+
+</body>
+</html>
