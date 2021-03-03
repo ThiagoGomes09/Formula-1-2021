730ec71fb08c61cdf5b39fe1f0c80e93c63c259d
Thiago Gomes <thiago.g.t9@hotmail.com>
2021-03-03 16:40:36 -0300

    Alteração de Cores

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <h1>Calendário</h1>
     <p>A temporada 2021 começa no próximo dia 28/03 com muitas novidades</p>
     <div style="width: 100%;display:flex;height: 80px">
-        <div style="width:50;background: blue"></div>
-        <div style="width:50;background: red"></div>
+        <div style="width:50%;background: blue"></div>
+        <div style="width:50%;background: red"></div>
     </div>
 </body>
 
